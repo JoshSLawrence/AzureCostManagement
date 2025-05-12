e8e2753f5b0a0536048af90366284e29e36874b3
Josh Lawrence <josh@joshlawrence.dev>
2025-05-12 19:11:13 -0400

    feat: inject per environment appsettings.json config to DI container

--- AzureCostManagement/Program.cs
+++ AzureCostManagement/Program.cs
@@ -2,7 +2,7 @@
 using Azure.ResourceManager;
 using AzureCostManagement.Interfaces;
 using AzureCostManagement.Services;
-
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Graph;
@@ -25,7 +25,15 @@ public class Program
 
     public static IHostBuilder HostBuilder(string[] args, DefaultAzureCredential credential)
     {
-        return Host.CreateDefaultBuilder(args).ConfigureServices((context, services) =>
+        return Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((context, config) =>
+            {
+                config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                      .AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json",
+                        optional: true, reloadOnChange: true);
+
+            })
+            .ConfigureServices((context, services) =>
         {
             services.AddScoped<IService, CostService>();
             services.AddSingleton(new GraphServiceClient(credential));
